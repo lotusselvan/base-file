13ada82b84cd8101bec21ddc6c299f24f9cefba7
lotusselvan <thamariselvanwin94@gmail.com>
2024-05-23 12:05:45 +0530

    code changes

--- src/test/java/Pack/NewTeams.java
+++ src/test/java/Pack/NewTeams.java
@@ -3,6 +3,9 @@ package Pack;
 public class NewTeams {
 	public static void main(String[] args) {
 		System.out.println("hi lotus");
+		System.out.println("mohan");
+		System.out.println("fiyaz");
+		
 	}
 
 }
