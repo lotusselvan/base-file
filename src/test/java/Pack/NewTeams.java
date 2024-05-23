42a503184d6e38b962f0edc0e8ab0690ca278f2d
lotusselvan <thamariselvanwin94@gmail.com>
2024-05-23 12:27:03 +0530

    conflict changes

--- src/test/java/Pack/NewTeams.java
+++ src/test/java/Pack/NewTeams.java
@@ -1,8 +1,16 @@
 package Pack;
 
 public class NewTeams {
+	public void details() {
+		int i =1;
+		 String name ="thamrai";
+		 
+
+	}
 	public static void main(String[] args) {
 		System.out.println("hi lotus");
+		
+		
 	}
 
 }
